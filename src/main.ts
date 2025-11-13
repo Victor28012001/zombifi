37c1204274667436e3ade4ee3140bf255ff0d258
Victor <ifeanyivictor615@gmail.com>
2025-11-13 14:15:02 +0100

    first comistser

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import "./polyfills.js";
 import { Game } from "./core/Game.js";
 import { SceneTransition } from "./core/SceneTransition.js";
 import { GameState } from "./core/GameState.js";
+import { Buffer } from "buffer";
+window.Buffer = Buffer;
 // import { mountCivicAuth } from "./utils/CivicAuthMount.jsx";
 
 const game = new Game();
--- vite.config.js
+++ vite.config.js
@@ -1,46 +1,64 @@
 import { defineConfig } from "vite";
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-import nodePolyfills from 'rollup-plugin-node-polyfills';
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
+import nodePolyfills from "rollup-plugin-node-polyfills";
 
 export default defineConfig({
   base: "./",
+  publicDir: "public",
   build: {
     outDir: "dist",
     sourcemap: true,
     target: "es2020",
     rollupOptions: {
       plugins: [
-        nodePolyfills({ crypto: true, process: true })
-      ]
-    }
+        // Full Node polyfills for Rollup phase
+        nodePolyfills(),
+      ],
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true, // Helps forge-light and other mixed modules
+    },
   },
-  publicDir: "public",
+
   define: {
     global: "globalThis",
-    'process.env.NODE_DEBUG': 'false'
+    "process.env.NODE_DEBUG": "false",
+    "process.env": {}, // ensure process.env exists at runtime
   },
+
   optimizeDeps: {
     include: [
-      "buffer"
+      "buffer",
+      "process",
+      "stream",
+      "util",
+      "crypto",
     ],
     esbuildOptions: {
       target: "es2020",
       define: {
-        global: "globalThis"
+        global: "globalThis",
       },
       plugins: [
         NodeGlobalsPolyfillPlugin({
           buffer: true,
-          process: true
-        })
-      ]
-    }
+          process: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
   },
+
   resolve: {
     alias: {
-      crypto: 'crypto-browserify',
-      stream: 'stream-browserify',
-      assert: 'assert-browserify'
-    }
-  }
+      // Force Vite to use browser-friendly versions
+      crypto: "crypto-browserify",
+      stream: "stream-browserify",
+      assert: "assert-browserify",
+      buffer: "buffer",
+      util: "util",
+      process: "process/browser",
+    },
+  },
 });
